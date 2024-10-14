507f10103721be932381784a85d901490b6726d2
YJS96 <wnt9864@hotmail.com>
2024-10-14 15:26:08 +0900

    :sparkles: Feat: 환자 홈 티켓 캐로셀

--- src/components/ui/carousel/CarouselNext.vue
+++ src/components/ui/carousel/CarouselNext.vue
@@ -1,24 +1,26 @@
 <script setup lang="ts">
-import { useCarousel } from './useCarousel'
-import type { WithClassAsProps } from './interface'
-import { cn } from '@/lib/utils'
-import { Button } from '@/components/ui/button'
+import { useCarousel } from './useCarousel';
+import type { WithClassAsProps } from './interface';
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
 
-const props = defineProps<WithClassAsProps>()
+const props = defineProps<WithClassAsProps>();
 
-const { orientation, canScrollNext, scrollNext } = useCarousel()
+const { orientation, canScrollNext, scrollNext } = useCarousel();
 </script>
 
 <template>
   <Button
     :disabled="!canScrollNext"
-    :class="cn(
-      'touch-manipulation border-none absolute h-8 w-8 rounded-full p-0',
-      orientation === 'horizontal'
-        ? 'right-0 top-1/2 -translate-y-1/2'
-        : '-bottom-12 left-1/2 -translate-x-1/2 rotate-90',
-      props.class,
-    )"
+    :class="
+      cn(
+        'touch-manipulation border-none absolute h-8 w-8 rounded-full p-0 hover:bg-opacity-0 hover:bg-transparent',
+        orientation === 'horizontal'
+          ? 'right-0 top-1/2 -translate-y-1/2'
+          : '-bottom-12 left-1/2 -translate-x-1/2 rotate-90',
+        props.class
+      )
+    "
     variant="outline"
     @click="scrollNext"
   >
