bb4fc1033a30198fd275d80ae5d5be7be123bf42
yjs96 <yimj57027@gmail.com>
2024-10-16 01:00:36 +0000

    :ambulance: Hotfix: 약사기준 처방전 리스트 추가(환자이름으로 변경해야함)

--- src/pages/pharmacist/PharmacistPage.vue
+++ src/pages/pharmacist/PharmacistPage.vue
@@ -15,12 +15,36 @@ import {
 import { QrcodeStream } from 'vue-qrcode-reader';
 import axiosInstance from '@/api/instance';
 import { toast } from '@steveyuowo/vue-hot-toast';
+import moment from 'moment';
 
 // QR 코드 데이터의 타입을 정의합니다.
 interface QRData {
   userId: string;
   prescriptionId: string;
   hello: string;
+}
+
+interface Prescription {
+  prescriptionPk: number;
+  prescriptionNo: number;
+  duration: number;
+  maxDate: number;
+  description: string;
+  prescriptionSt: boolean;
+  hospitalNm: string;
+  hospitalSi: string;
+  hospitalGu: string;
+  hospitalDong: string;
+  hospitalDetailAddress: string | null;
+  pharmacyNm: string;
+  diseaseList: Disease[];
+  createYmd: string;
+}
+
+interface Disease {
+  diseasePk: number;
+  diseaseCd: string;
+  diseaseNm: string;
 }
 
 // Vue Router를 사용하기 위한 설정
@@ -30,7 +54,140 @@ const router = useRouter();
 const isDialogOpen = ref(false);
 
 // 최근 처방전 목록을 저장하는 ref
-// const recentPrescriptions = ref([]);
+const recentPrescriptions = ref<Prescription[]>([
+  {
+    prescriptionPk: 26,
+    prescriptionNo: 10,
+    duration: 3,
+    maxDate: 1,
+    description: '식이요법에 주의하세요 1형 당뇨 환자입니다',
+    prescriptionSt: true,
+    hospitalNm: '그대의봄여성의원2',
+    hospitalSi: '경기도',
+    hospitalGu: '과천시',
+    hospitalDong: '과천동',
+    hospitalDetailAddress: null,
+    pharmacyNm: '김성헌 약국',
+    diseaseList: [
+      {
+        diseasePk: 2054,
+        diseaseCd: 'E10',
+        diseaseNm: '1형 당뇨병'
+      }
+    ],
+    createYmd: '2024-10-15T19:19:44'
+  },
+  {
+    prescriptionPk: 25,
+    prescriptionNo: 9,
+    duration: 3,
+    maxDate: 1,
+    description: '당뇨병임',
+    prescriptionSt: true,
+    hospitalNm: '그대의봄여성의원2',
+    hospitalSi: '경기도',
+    hospitalGu: '과천시',
+    hospitalDong: '과천동',
+    hospitalDetailAddress: null,
+    pharmacyNm: '김성헌 약국',
+    diseaseList: [
+      {
+        diseasePk: 2054,
+        diseaseCd: 'E10',
+        diseaseNm: '1형 당뇨병'
+      }
+    ],
+    createYmd: '2024-10-15T19:15:34'
+  },
+  {
+    prescriptionPk: 24,
+    prescriptionNo: 8,
+    duration: 3,
+    maxDate: 4,
+    description: '대체 조제 불가',
+    prescriptionSt: true,
+    hospitalNm: '청구경희한의원 건대',
+    hospitalSi: '서울특별시',
+    hospitalGu: '광진구',
+    hospitalDong: '화양동',
+    hospitalDetailAddress: '111-47',
+    pharmacyNm: '김성헌 약국',
+    diseaseList: [
+      {
+        diseasePk: 73,
+        diseaseCd: 'A15.1',
+        diseaseNm: '배양만으로 확인된 폐결핵'
+      }
+    ],
+    createYmd: '2024-10-15T16:07:40'
+  },
+  {
+    prescriptionPk: 23,
+    prescriptionNo: 7,
+    duration: 3,
+    maxDate: 3,
+    description: '대체 조제 불가',
+    prescriptionSt: true,
+    hospitalNm: '청구경희한의원 건대',
+    hospitalSi: '서울특별시',
+    hospitalGu: '광진구',
+    hospitalDong: '화양동',
+    hospitalDetailAddress: '111-47',
+    pharmacyNm: '김성헌 약국',
+    diseaseList: [
+      {
+        diseasePk: 2056,
+        diseaseCd: 'E10.1',
+        diseaseNm: '산증을 동반한 1형 당뇨병'
+      }
+    ],
+    createYmd: '2024-10-15T16:04:23'
+  },
+  {
+    prescriptionPk: 21,
+    prescriptionNo: 5,
+    duration: 3,
+    maxDate: 0,
+    description: '대체 조제 불가',
+    prescriptionSt: true,
+    hospitalNm: '청구경희한의원 건대',
+    hospitalSi: '서울특별시',
+    hospitalGu: '광진구',
+    hospitalDong: '화양동',
+    hospitalDetailAddress: '111-47',
+    pharmacyNm: '김성헌 약국',
+    diseaseList: [
+      {
+        diseasePk: 2056,
+        diseaseCd: 'E10.1',
+        diseaseNm: '산증을 동반한 1형 당뇨병'
+      }
+    ],
+    createYmd: '2024-10-15T13:00:46'
+  },
+  {
+    prescriptionPk: 20,
+    prescriptionNo: 4,
+    duration: 3,
+    maxDate: 3,
+    description: '대체 조제 불가',
+    prescriptionSt: true,
+    hospitalNm: '청구경희한의원 건대',
+    hospitalSi: '서울특별시',
+    hospitalGu: '광진구',
+    hospitalDong: '화양동',
+    hospitalDetailAddress: '111-47',
+    pharmacyNm: '김성헌 약국',
+    diseaseList: [
+      {
+        diseasePk: 4,
+        diseaseCd: 'A00.9',
+        diseaseNm: '상세불명의 콜레라'
+      }
+    ],
+    createYmd: '2024-10-15T12:50:01'
+  }
+]);
 
 // QR 코드가 감지되었을 때 실행되는 함수
 async function onDetect(detectedCodes: Array<{ rawValue: string }>) {
@@ -73,7 +230,8 @@ async function validateQRInfo(qrData: QRData) {
 const fetchRecentPrescriptions = async () => {
   try {
     const response = await axiosInstance.get('/api/pharmacy/list?pageIndex=0&pageSize=10');
-    console.log(response.data.data);
+    // console.log(response.data.data.prescriptionList);
+    recentPrescriptions.value = response.data.data.prescriptionList;
     // recentPrescriptions.value = response.data.map((prescription) => ({
     //   id: prescription.prescriptionId,
     //   name: prescription.patientName,
@@ -120,9 +278,8 @@ onMounted(() => {
           <QrcodeStream @detect="onDetect" @error="onError" />
         </DialogContent>
       </Dialog>
-      {{}}
 
-      <!-- <ShadowBox :padding-x="20" :padding-y="20" :margin-bottom="0">
+      <ShadowBox :padding-x="20" :padding-y="20" :margin-bottom="0">
         <div class="recent-list-title">
           <div>최근 처방전 내역</div>
         </div>
@@ -130,23 +287,25 @@ onMounted(() => {
         <div class="recent-list">
           <div
             v-for="prescription in recentPrescriptions"
-            :key="prescription.id"
+            :key="prescription.prescriptionPk"
             class="recent-item"
+            @click="$router.push(`/pharmacist/${prescription.prescriptionPk}`)"
           >
             <div class="recent-info">
               <div class="recent-group">
-                <i class="fa-regular fa-file"></i>
-                <div class="recent-name">{{ prescription.name }} 환자님</div>
+                <div class="recent-name">{{ prescription.prescriptionPk }} 환자님</div>
               </div>
-              <div class="recent-date">{{ prescription.date }}</div>
+              <div class="recent-date">
+                <i class="fa-regular fa-calendar"></i
+                >{{ moment(prescription.createYmd).format('YY년 MM월 DD일 HH:MM') }}
+              </div>
             </div>
             <div class="recent-status">
               <i class="fa-solid fa-chevron-right"></i>
-              <span class="status-text">{{ prescription.status }}</span>
             </div>
           </div>
         </div>
-      </ShadowBox> -->
+      </ShadowBox>
     </div>
   </Main>
 </template>
@@ -231,7 +390,8 @@ onMounted(() => {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin-bottom: 20px;
+  margin-bottom: 28px;
+  padding: 0 4px;
 }
 
 .recent-item:last-child {
@@ -250,14 +410,14 @@ onMounted(() => {
 }
 
 .recent-name {
-  font-size: 16px;
+  font-size: 20px;
+  font-weight: 600;
 }
 
 .recent-date {
   font-size: 14px;
   color: var(--dark-gray);
   margin-top: 4px;
-  margin-left: 12px;
 }
 
 .recent-status {
@@ -270,5 +430,9 @@ onMounted(() => {
 .status-text {
   font-size: 14px;
   color: var(--kb-yellow);
+}
+
+.fa-calendar {
+  margin-right: 6px;
 }
 </style>
